6242db783d760156a4bbe78c503347f4e7c42b6a
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2026-06-11 16:59:36 +0500

    created a form and added some blog on my own

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.post_list, name="post_list"),
+    path("new/", views.post_create, name="post_create"),
     path("<int:post_id>/", views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import PostForm
 from .models import Post
 
 
@@ -11,3 +12,14 @@ def post_list(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, "blog/post_detail.html", {"post": post})
+
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect("post_detail", post_id=post.id)
+    else:
+        form = PostForm()
+    return render(request, "blog/post_form.html", {"form": form})
